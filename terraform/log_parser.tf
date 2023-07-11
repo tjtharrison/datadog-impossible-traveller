324d181c10022ea884d458db9bdc70ad22cdc933
tjtharrison <gh@tjth.co>
2023-07-11 19:56:40 +0100

    fix: Fixed differences in grok parser after escapes

--- terraform/log_parser.tf
+++ terraform/log_parser.tf
@@ -7,7 +7,7 @@ resource "datadog_logs_custom_pipeline" "demo_pipeline" {
   processor {
     grok_parser {
       is_enabled = true
-      name       = "My app"
+      name       = "Grok Parser"
       samples    = [
         "{\"time\":\"2023-07-10 20:30:11\", \"name\": \"root\", \"level\": \"INFO\", \"message\": \"Generating logs..\" \"username\": \"tjtharrison\", \"source_ip\": \"0.0.0.0\"}",
       ]
@@ -15,7 +15,7 @@ resource "datadog_logs_custom_pipeline" "demo_pipeline" {
 
       grok {
         support_rules = ""
-        match_rules = "autoFilledRule1 \\{\"time\"\\:\"%%{date(\"yyyy-MM-dd HH:mm:ss\"):date}\",\\s+\"name\"\\:\\s+\"root\",\\s+\"level\"\\:\\s+\"%%{word:level}\",\\s+\"message\"\\:\\s+\"Generating\\s+logs\\.\\.\"\\s+\"username\"\\:\\s+\"%%{word:username}\",\\s+\"source_ip\"\\:\\s+\"%%{ipv4:source_ip}\"\\} \\n"
+        match_rules = "autoFilledRule1 \\{\\\"time\\\"\\:\\\"%%{date(\"yyyy-MM-dd HH:mm:ss\"):date}\\\",\\s+\\\"name\\\"\\:\\s+\\\"root\\\",\\s+\\\"level\\\"\\:\\s+\\\"%%{word:level}\\\",\\s+\\\"message\\\"\\:\\s+\\\"Generating\\s+logs\\.\\.\\\"\\s+\\\"username\\\"\\:\\s+\\\"%%{word:username}\\\",\\s+\\\"source_ip\\\"\\:\\s+\\\"%%{ipv4:network.client.ip}\\\"\\}"
       }
     }
   }
